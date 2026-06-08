1fd7290134288aba747c11d04c678896f5dc2c9f
Oleksandr Shkabbura <shkabura007@gmail.com>
2026-06-08 15:16:53 +0300

    add cache decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,14 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+    def wrapper(*args):
+        if args in storage:
+            print("Getting from cache")
+            return storage[args]
+        else:
+            storage[args] = func(*args)
+            print("Calculating new result")
+            return storage[args]
+
+    return wrapper
